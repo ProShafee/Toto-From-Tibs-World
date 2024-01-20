33dcfac765447bd0a2d78d54edeeac0728beafe4
Shafee Rehman <30435924+ProShafee@users.noreply.github.com>
2024-01-20 21:48:55 +1300

    fastlane init

---
+++ Toto From Tibs World/Toto From Tibs World/Toto_From_Tibs_WorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  Toto_From_Tibs_WorldApp.swift
+//  Toto From Tibs World
+//
+//  Created by Shafee Rehman on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Toto_From_Tibs_WorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
